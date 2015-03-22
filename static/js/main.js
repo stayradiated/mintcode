0ee0e22bb19bad791a513808cf7bce0fa2645058
George Czabania <george@czabania.com>
2015-03-22 19:33:53 +1300

    Fix up main.js and masonry

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,30 @@
-var msnry;
+'use strict';
 
-function triggerMasonry() {
-  // don't proceed if masonry has not been initialized
-  if (! msnry) { return; }
-  msnry.layout();
-}
+(function () {
+  var msnry;
 
-// initialize masonry on document ready
-docReady(function() {
-  msnry = new Masonry('.project-section', {
-    gutter: 10,
-    itemSelector: '.project'
+  if (! window.hasOwnProperty('Masonry')) {
+    Typekit.load();
+    return;
+  }
+
+  function triggerMasonry() {
+    // don't proceed if masonry has not been initialized
+    if (! msnry) { return; }
+    msnry.layout();
+  }
+
+  // initialize masonry on document ready
+  docReady(function() {
+    msnry = new Masonry('.project-section', {
+      gutter: 10,
+      itemSelector: '.project'
+    });
   });
-});
 
-// trigger masonry when fonts have loaded
-Typekit.load({
-  active: triggerMasonry,
-  inactive: triggerMasonry
-});
+  // trigger masonry when fonts have loaded
+  Typekit.load({
+    active: triggerMasonry,
+    inactive: triggerMasonry
+  });
+}());
